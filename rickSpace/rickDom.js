f7b61dd542c90332da6acca46de9c845c9f1f319
rickandrew2 <rickandrew.macapagal@gmail.com>
2025-03-06 22:08:56 +0800

    test

--- rickSpace/rickDom.js
+++ rickSpace/rickDom.js
@@ -1,6 +1,9 @@
 DOM.insertAdjacentHTML('beforeend', `
     <div class="vh-100 d-flex flex-column justify-content-center align-items-center bg-secondary">
+        <div class="bg-light p-5 rounded">
+        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="Rick" class="img-fluid rounded-circle">
         <h1>RICK SPACE</h1>
         <p>This where you will work</p>
+        </div>
     </div>
     `);

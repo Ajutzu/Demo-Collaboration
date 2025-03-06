1240310dad6cd31d1125f78ec21ee0cc72b6d001
Joshua Andrei Samonte <joshsamonte021@gmail.com>
2025-03-06 20:38:23 +0800

    try branch

--- joshuaSpace/joshuaDom.js
+++ joshuaSpace/joshuaDom.js
@@ -2,5 +2,6 @@ DOM.insertAdjacentHTML('beforeend', `
     <div class="vh-100 d-flex flex-column justify-content-center align-items-center bg-warning">
         <h1>JOSHUA SPACE</h1>
         <p>This where you will work</p>
+        <h2>TRY BRANCH</h2>
     </div>
     `);

c9cb57e0b3aa74c0a89542c57fc6019fcc3b9baf
Aeron James A. Castillo <aeronjamescastillo6@gmail.com>
2025-03-07 11:16:47 +0800

    update my space rick to

--- rickSpace/rickDom.js
+++ rickSpace/rickDom.js
@@ -1,11 +1,13 @@
 DOM.insertAdjacentHTML('beforeend', `
     <div class="vh-100 d-flex flex-column justify-content-center align-items-center bg-secondary">
-        <div class="bg-light p-5 rounded">
-        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="Rick" class="img-fluid rounded-circle">
-        <h1>RICK SPACE</h1>
+        <div class="bg-light p-5 rounded text-center">
+        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="Rick" class="img-fluid rounded-circle shadow-lg border">
+        <img src="https://media.giphy.com/media/3oriO0OEd9QIDdllqo/giphy.gif" alt="Meme" width="300" class="rounded-circle shadow-lg border border-dark">
+       
+        <h1 class="mt-5">RICK SPACE</h1>
         <p>This where you will work</p>
         <p>Test>Test>Test</p>
-        <img src="https://media.giphy.com/media/3oriO0OEd9QIDdllqo/giphy.gif" alt="Meme" width="500" class="rounded shadow-lg border border-dark">
+
         </div>
     </div>
     `);

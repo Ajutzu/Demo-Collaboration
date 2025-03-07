ae7e8a62f52ce783c3bb4ee045a192247bec5a0e
Mhark Anthony <82895726+ynodev@users.noreply.github.com>
2025-03-08 01:35:39 +0800

    Mhark Initial Commit

--- mharkSpace/mharkDom.js
+++ mharkSpace/mharkDom.js
@@ -1,6 +1,8 @@
 DOM.insertAdjacentHTML('beforeend', `
-    <div class="vh-100 d-flex flex-column justify-content-center align-items-center bg-primary">
-        <h1>MHARK SPACE</h1>
-        <p>This where you will work</p>
+    <div class="vh-100 d-flex flex-column justify-content-center align-items-center" style="background-color: #181AFF;">
+        <h1 class="text-white">Test</h1>
+        <img src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExank1b21lNmJ1eTNsbWt0YTVjZ2Qwam8xcWI2dW16OXZsMXQ4aGltMiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/l0MYyWsTzhEeL5MA0/giphy.gif" 
+             alt="" 
+             class="rounded-3" />
     </div>
-    `);
+`);

40b0016c8906c3020a8acdfd0ec4bb74f608dd17
Aeron James A. Castillo <aeronjamescastillo6@gmail.com>
2025-03-07 11:26:19 +0800

    update

--- daryllSpace/daryllDom.js
+++ daryllSpace/daryllDom.js
@@ -1,11 +1,20 @@
 DOM.insertAdjacentHTML('beforeend', `
-    <div class="row vh-100 d-flex justify-content-center align-items-center bg-danger">
-        <div class="col-md-6 col-sm-12 d-flex justify-content-center">
-            <img src="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExZDl3bmdkYjVyaDZrbHR2eWF0dmkxaW9sbmx5NmI0MDE2b2UzMnMwYSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/B4jfJqiIxvU08/giphy.gif" alt="image testing" class="img-fluid">
-        </div>
-        <div class="col-md-6 col-sm-12 d-flex flex-column justify-content-center align-items-center">
-            <h2 style="color:rgb(15, 18, 21); font-weight: 600;">TEST TEST</h2>
-            <p style="color:rgb(15, 18, 21); text-align: justify; width: 75%">Fliplove, game? Tutok na tutok sa lips mo ang kiss ko bang. Taktak bungo, tagaktak dugo. Pero hindi ito inlove na nagmamahal mahalan. Paano manlalamig e wala namang iloveyou-han</p>
+    <div class="container-fluid">
+        <div class="row vh-100 d-flex justify-content-center align-items-center bg-danger">
+            <div class="col-md-6 col-sm-12 d-flex justify-content-center">
+                <img src="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExZDl3bmdkYjVyaDZrbHR2eWF0dmkxaW9sbmx5NmI0MDE2b2UzMnMwYSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/B4jfJqiIxvU08/giphy.gif" 
+                     alt="image testing" 
+                     class="img-fluid" 
+                     style="max-width: 100%;">
+            </div>
+            <div class="col-md-6 col-sm-12 d-flex flex-column justify-content-center align-items-center text-center text-md-start">
+                <h2 class="fw-bold text-dark">TEST TEST</h2>
+                <p class="text-dark w-100 w-md-75">
+                    Fliplove, game? Tutok na tutok sa lips mo ang kiss ko bang. 
+                    Taktak bungo, tagaktak dugo. Pero hindi ito inlove na nagmamahal mahalan. 
+                    Paano manlalamig e wala namang iloveyou-han.
+                </p>
+            </div>
         </div>
     </div>
-    `);
+`);

1b3bf6601a9a2feb36a133f8f00387199a172804
robbie272002 <robertojrvelasco359@gmail.com>
2025-03-08 19:15:11 +0800

    Updated robbieDom.js

--- robbieSpace/robbieDom.js
+++ robbieSpace/robbieDom.js
@@ -1,7 +1,6 @@
 DOM.insertAdjacentHTML('beforeend', `
     <div class="vh-100 d-flex flex-column justify-content-center align-items-center bg-black text-white">
         <h1>Hi! This is Robbie</h1>
-        <p>This where you will work</p>
         <div class="tenor-gif-embed" data-postid="9878940352483067214" data-share-method="host" data-aspect-ratio="1.23267" data-width="100%">
         <a href="https://tenor.com/view/alarm-gif-9878940352483067214">Alarm GIF</a>from <a href="https://tenor.com/search/alarm-gifs">Alarm GIFs</a></div> <script type="text/javascript" async src="https://tenor.com/embed.js"></script>
     </div>
